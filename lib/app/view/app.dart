71a8e21bf8cb8f3d81de0ca06a10dd3785e4a11a
OmarElDwary <omareldwary91@gmail.com>
2025-03-03 23:39:57 +0200

    add checkin and out btns

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:gps_attendance_system/counter/counter.dart';
 import 'package:gps_attendance_system/l10n/l10n.dart';
-
+import 'package:gps_attendance_system/presentation/screens/user-check/check_in.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -17,7 +16,7 @@ class App extends StatelessWidget {
       ),
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
-      home: const CounterPage(),
+      home: const CheckIn(),
     );
   }
 }
---
+++ lib/presentation/screens/user-check/check_in.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/widgets.dart';
+import 'package:gps_attendance_system/presentation/screens/user-check/widgets/buttons.dart';
+
+class CheckIn extends StatefulWidget {
+  const CheckIn({super.key});
+
+  @override
+  State<CheckIn> createState() => _CheckInState();
+}
+
+class _CheckInState extends State<CheckIn> {
+  void checkIn() {}
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Row(
+            children: [
+              CheckInOutButton(
+                label: 'Check In',
+                color: const Color(0XFF2563EB),
+                onPressed: checkIn,
+              ),
+              CheckInOutButton(
+                label: 'Check Out',
+                color: const Color(0XFF203546),
+                onPressed: checkIn,
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
