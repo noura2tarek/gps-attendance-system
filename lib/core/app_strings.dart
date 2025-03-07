6eb4c22a7d98970fa17ab706f652140a92bfca8a
Noura Tarek <99992263+noura2tarek@users.noreply.github.com>
2025-03-07 02:37:00 +0200

    Edit strings

--- lib/core/app_strings.dart
+++ lib/core/app_strings.dart
@@ -5,7 +5,7 @@ class AppStrings {
   static const String managers = 'Managers';
   static const String geofence = 'Geofence';
   static const String settings = 'Settings';
-      static const String profile = 'Profile';
+  static const String profile = 'Profile';
   static const String leaves = 'Leaves';
   static const String logout = 'Logout';
   static const String viewAll = 'View All';
