d99af7625d019680f441f96d0ea3a03445f6ba45
Noura Tarek <99992263+noura2tarek@users.noreply.github.com>
2025-03-09 02:16:26 +0200

    Edit custom list tile

--- lib/presentation/screens/admin_dashboard/widgets/custom_list_tile.dart
+++ lib/presentation/screens/admin_dashboard/widgets/custom_list_tile.dart
@@ -4,25 +4,58 @@ import 'package:gps_attendance_system/core/themes/app_colors.dart';
 class CustomListTile extends StatelessWidget {
   const CustomListTile({
     required this.title,
-    required this.widget,
+    this.subtitle,
     super.key,
     this.onTap,
+    this.leadingWidget,
+    this.isUser = true,
   });
 
   final String title;
-  final Widget widget;
+  final String? subtitle;
   final void Function()? onTap;
+  final bool isUser;
+  final Widget? leadingWidget;
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      focusColor: AppColors.secondary,
-      hoverColor: AppColors.secondary,
-      selectedColor: AppColors.secondary,
-      iconColor: AppColors.thirdMintGreen,
-      title: Text(title),
-      leading: widget,
-      onTap: onTap,
-    );
+    double avatarRadius = MediaQuery.of(context).size.width * 0.07;
+    return isUser
+        ? Card(
+            color: Colors.white.withValues(alpha: 0.9),
+            surfaceTintColor: Colors.white,
+            elevation: 1.5,
+            child: ListTile(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
+              focusColor: AppColors.secondary,
+              hoverColor: AppColors.secondary,
+              selectedColor: AppColors.secondary,
+              iconColor: AppColors.thirdMintGreen,
+              title: Text(
+                title,
+                style: const TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 16,
+                ),
+              ),
+              subtitle: subtitle == null ? null : Text(subtitle ?? ''),
+              leading: CircleAvatar(
+                backgroundColor: AppColors.primary,
+                radius: avatarRadius,
+              ),
+              onTap: onTap,
+            ),
+          )
+        : ListTile(
+            focusColor: AppColors.secondary,
+            hoverColor: AppColors.secondary,
+            selectedColor: AppColors.secondary,
+            iconColor: AppColors.thirdMintGreen,
+            title: Text(title),
+            leading: leadingWidget,
+            onTap: onTap,
+          );
   }
 }
